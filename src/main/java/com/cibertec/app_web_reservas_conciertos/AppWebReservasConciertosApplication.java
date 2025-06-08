30775e1af1e7b59ab69a0453d5bbe92672c15179
joqv <joseanqv@gmail.com>
2025-06-08 15:28:00 -0500

    variable de entorno, config de run de app

--- src/main/java/com/cibertec/app_web_reservas_conciertos/AppWebReservasConciertosApplication.java
+++ src/main/java/com/cibertec/app_web_reservas_conciertos/AppWebReservasConciertosApplication.java
@@ -1,5 +1,6 @@
 package com.cibertec.app_web_reservas_conciertos;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppWebReservasConciertosApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(AppWebReservasConciertosApplication.class, args);
 	}
 
